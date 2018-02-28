bbe840bc7baa929f3aff7163a158d43f0d4a5d67
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-02-27 18:03:00 -0600

    Add CUDA check, remove trailing whitespace

--- nussl/separation/deep_clustering.py
+++ nussl/separation/deep_clustering.py
@@ -12,31 +12,31 @@ import masks
 from resampy import resample
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
-import copy
+import copy, warnings
 
 class DeepClustering(mask_separation_base.MaskSeparationBase):
     """Implements deep clustering for source separation, using PyTorch.
-    
+
     Deep clustering is a deep learning approach to source separation. It takes as input a mel-spectrogram
     representation of an audio mixture. Each time-frequency bin is mapped into an K-dimensional embedding.
     The model works out so that time-frequency bins that are dominated by different sources map
     to embeddings that are distant, and bins that are dominated by the same source map to embeddings that are
     near. The sources are then recovered using K-Means clustering on the embedding space.
-    
+
     References:
 
-    Hershey, J. R., Chen, Z., Le Roux, J., & Watanabe, S. (2016, March). 
-    Deep clustering: Discriminative embeddings for segmentation and separation. 
-    In Acoustics, Speech and Signal Processing (ICASSP), 
+    Hershey, J. R., Chen, Z., Le Roux, J., & Watanabe, S. (2016, March).
+    Deep clustering: Discriminative embeddings for segmentation and separation.
+    In Acoustics, Speech and Signal Processing (ICASSP),
     2016 IEEE International Conference on (pp. 31-35). IEEE.
 
-    Luo, Y., Chen, Z., Hershey, J. R., Roux, J. L., & Mesgarani, N. (2016). 
-    Deep Clustering and Conventional Networks for Music Separation: Stronger Together. 
+    Luo, Y., Chen, Z., Hershey, J. R., Roux, J. L., & Mesgarani, N. (2016).
+    Deep Clustering and Conventional Networks for Music Separation: Stronger Together.
     arXiv preprint arXiv:1611.06265.
-    
+
     Example:
-        music = AudioSignal("/media/ext/datasets/DSD100/Mixtures/Test/021 - James May - On The Line/mixture.wav", 
-                            offset=45, 
+        music = AudioSignal("/media/ext/datasets/DSD100/Mixtures/Test/021 - James May - On The Line/mixture.wav",
+                            offset=45,
                             duration=20)
 
         music.stft_params.window_length = 2048
@@ -48,11 +48,11 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
         plt.figure(figsize=(20, 8))
         separation.plot()
         plt.tight_layout()
-        plt.show()    
+        plt.show()
     """
-    def __init__(self, input_audio_signal, 
+    def __init__(self, input_audio_signal,
                  mask_type=mask_separation_base.MaskSeparationBase.SOFT_MASK,
-                 model_path='/media/ext/models/deep_clustering_vocal_44k_long.model', 
+                 model_path='/media/ext/models/deep_clustering_vocal_44k_long.model',
                  num_sources=2,
                  num_layers=4,
                  hidden_size=500,
@@ -64,35 +64,41 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
                  use_librosa_stft=False,
                  cutoff=-40):
         super(DeepClustering, self).__init__(input_audio_signal=input_audio_signal, mask_type=mask_type)
-        
+
         self.resample_rate = resample_rate
         if self.audio_signal.sample_rate != self.resample_rate:
             self.audio_signal.resample(self.resample_rate)
-            
+
         self.use_librosa_stft = use_librosa_stft
         self.num_mels = num_mels
         self.num_sources = num_sources
         self.num_fft = self.audio_signal.stft_params.n_fft_bins
         self.mel_filter_bank = librosa.filters.mel(self.resample_rate, self.num_fft, self.num_mels).T
-        self.inverse_mel_filter_bank = np.linalg.pinv(self.mel_filter_bank) 
+        self.inverse_mel_filter_bank = np.linalg.pinv(self.mel_filter_bank)
         self.max_distance = max_distance
-        
+        self.use_cuda = torch.cuda.is_available()
+        if not self.use_cuda:
+            warnings.warn('CUDA not available, running on CPU may be slow')
+
         self.stft = None
         self.mel_spectrogram = None
         self.silence_mask = None
         self.cutoff = cutoff
         self.model = TransformerDeepClustering(num_layers=num_layers,
                                  hidden_size=hidden_size,
-                                embedding_size=embedding_size).cuda()
+                                embedding_size=embedding_size)
+        if self.use_cuda:
+            self.model.cuda()
+
         self.load_model(model_path)
         self.clusterer = KMeans(n_clusters=self.num_sources)
         self.embeddings = None
-        
+
         self.do_mono = do_mono
 
         if self.do_mono:
             self.audio_signal.to_mono(overwrite=True)
-        
+
     def load_model(self, model_path):
         self.model.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
         self.model.eval()
@@ -101,33 +107,37 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
         self.stft = self.audio_signal.stft(overwrite=True, remove_reflection=True, use_librosa=self.use_librosa_stft)
         magnitude = np.abs(self.stft)
         self.mel_spectrogram = np.empty((self.audio_signal.num_channels, self.stft.shape[1], self.num_mels))
-        
+
         for i in range(self.audio_signal.num_channels):
             self.mel_spectrogram[i, :, :] = np.dot(magnitude[:, :, i].T, self.mel_filter_bank)
-            
+
         self.mel_spectrogram = 10.0 * np.log10(self.mel_spectrogram**2 + 1e-7)
         self.silence_mask = self.mel_spectrogram > self.cutoff
         self.mel_spectrogram -= np.mean(self.mel_spectrogram)
         self.mel_spectrogram /= np.std(self.mel_spectrogram) + 1e-7
-    
+
     def deep_clustering(self):
-        input_data = Variable(torch.FloatTensor(self.mel_spectrogram)).cuda()
+        input_data = Variable(torch.FloatTensor(self.mel_spectrogram))
+
+        if self.use_cuda:
+            input_data.cuda()
+
         if self.embeddings is None:
             embeddings = self.model(input_data)
             self.embeddings = embeddings.view(-1, embeddings.size(-1)).cpu().data.numpy()
         self.clusterer.fit(self.embeddings)
-        
+
         assignments = self.clusterer.labels_ + 1
-        self.assignments = assignments.reshape(self.mel_spectrogram.shape) 
+        self.assignments = assignments.reshape(self.mel_spectrogram.shape)
         self.centroids = self.clusterer.cluster_centers_
-        
-            
+
+
     def _extract_masks(self, ch):
         if self.audio_signal.stft_data is None:
             raise ValueError('Cannot extract masks with no signal_stft data')
-            
+
         channel_mask_list = []
-        
+
         for cluster_index in range(1, self.num_sources + 1):
             if self.mask_type == self.BINARY_MASK:
                 mask = ((self.silence_mask[ch, :, :] * self.assignments[ch, :, :]) == cluster_index)
@@ -142,7 +152,7 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
             mask += np.abs(mask.min())
             mask /= (np.max(mask) + 1e-7)
             channel_mask_list.append(mask)
-        
+
         if self.mask_type == self.SOFT_MASK:
             data = np.stack(channel_mask_list, axis=-1)
             original_shape = data.shape
@@ -153,33 +163,33 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
 
         return channel_mask_list
 
-    
+
     def run(self):
         self._compute_spectrograms()
         self.deep_clustering()
-        
+
         uncollated_masks = []
         for i in range(self.audio_signal.num_channels):
             uncollated_masks += self._extract_masks(i)
-        
-        collated_masks = [np.dstack([uncollated_masks[s + ch * (self.num_sources)] 
+
+        collated_masks = [np.dstack([uncollated_masks[s + ch * (self.num_sources)]
                                      for ch in range(self.audio_signal.num_channels)])
                           for s in range(self.num_sources)]
-        
+
         self.masks = []
-        
+
         for mask in collated_masks:
             if self.mask_type == self.BINARY_MASK:
                 mask = np.round(mask)
                 mask_object = masks.BinaryMask(mask)
             elif self.mask_type == self.SOFT_MASK:
-                
+
                 mask_object = masks.SoftMask(mask)
             else:
                 raise ValueError('Unknown mask type {}!'.format(self.mask_type))
             self.masks.append(mask_object)
         return self.masks
-      
+
     def make_audio_signals(self):
         """ Applies each mask in self.masks and returns a list of audio_signal objects for each source.
         Returns:
@@ -194,15 +204,15 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
             self.sources.append(source)
 
         return self.sources
-    
+
     def project_embeddings(self, num_dimensions):
         transform = PCA(n_components=num_dimensions)
         output_transform = transform.fit_transform(self.embeddings)
         return output_transform
-        
+
     def plot(self):
         """ Plots relevant information for deep clustering onto the active figure, given by matplotlib.pyplot.figure()
-            outside of this function. The three plots are: 
+            outside of this function. The three plots are:
                 1. PCA of emeddings onto 2 dimensions for visualization
                 2. The mixture mel-spectrogram.
                 3. The source assignments of each tf-bin in the mixture spectrogram.
@@ -216,20 +226,20 @@ class DeepClustering(mask_separation_base.MaskSeparationBase):
         plt.xticks([])
         plt.ylabel('Frequency (mel)')
         plt.title('Mixture')
-        
+
         plt.subplot(grid[1:-1, :3])
-        
+
         xmin = output_transform[:, 0].min()
         xmax = output_transform[:, 0].max()
         ymin = output_transform[:, 1].min()
         ymax = output_transform[:, 1].max()
-        
-        plt.hexbin(output_transform[:, 0], output_transform[:, 1], bins='log', gridsize=100) 
+
+        plt.hexbin(output_transform[:, 0], output_transform[:, 1], bins='log', gridsize=100)
         plt.axis([xmin, xmax, ymin, ymax])
         plt.xlabel('PCA dim 1')
         plt.ylabel('PCA dim 2')
         plt.title('Embedding visualization')
-                        
+
         plt.subplot(grid[3:, 3:])
         plt.imshow(np.max(self.silence_mask * self.assignments, axis=0).T, origin='lower', aspect='auto', cmap='Greys')
         plt.xlabel('Time (frames)')
